003fe629e2c1c1e0184f5276bb46e9fee9d14e88
RoycetheGreat2 <165749717+RoycetheGreat2@users.noreply.github.com>
2024-12-09 14:12:45 +0000

    not empty

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Library Management System!");
+        System.out.println("Please select an option:");
+        System.out.println("1. Register as a new user");
+        System.out.println("2. Login as an existing user");
+        System.out.println("3. Exit the system");
+        System.out.println("Choice: ");
+        int choice = scanner.nextInt();
+        System.out.println(choice);
+        scanner.close();
+}   
+}
